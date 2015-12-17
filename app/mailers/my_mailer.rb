ddc5028e9aefaa2403fd25bf1d7d18311e4632c9
Todd Schwartzfarb <todd@wirestreak.com>
2015-12-16 22:07:49 -0500

    change

--- app/mailers/my_mailer.rb
+++ app/mailers/my_mailer.rb
@@ -23,7 +23,7 @@ class MyMailer < ActionMailer::Base
       @user.intelligence.to_i
 
     #send mail
-    mail(to: @user.email, subject: 'Submission for Trim Code Challenge')
+    mail(to: 'todd@wirestreak.com', subject: 'Submission for Trim Code Challenge')
   end
 
 end
